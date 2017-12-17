b78e4a15a1e2a1c6b4c7dd927ab18e86a2597e68
inspoy <inspoy@163.com>
2017-12-17 23:24:33 +0800

    完成TCP客户端第一版

--- DataTracker/TcpClient.cs
+++ DataTracker/TcpClient.cs
@@ -26,6 +26,81 @@ namespace Instech.DataTracker
 
         private IPEndPoint m_ipEnd;
         private Socket m_socket;
+        private Action<string> m_clientCallback;
+        private Action<string> m_errorCallback;
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="ip">服务端IP地址</param>
+        /// <param name="port">服务端端口号</param>
+        /// <param name="clientCallback">接收数据的回调</param>
+        /// <param name="stateCallback">接收连接结果的回调</param>
+        public void Init(
+            string ip,
+            int port,
+            Action<string> clientCallback,
+            Action<string> errorCallback,
+            Action<bool> stateCallback)
+        {
+            m_ipEnd = new IPEndPoint(IPAddress.Parse(ip), port);
+            m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            m_clientCallback = clientCallback;
+            m_errorCallback = errorCallback;
+            m_isReady = false;
+            totalSendLength = 0;
+            m_socket.BeginConnect(m_ipEnd, result =>
+            {
+                try
+                {
+                    m_socket.EndConnect(result);
+                    m_isReady = true;
+                    stateCallback(true);
+                }
+                catch (Exception)
+                {
+                    stateCallback?.Invoke(false);
+                }
+            }, null);
+        }
+
+        /// <summary>
+        /// 关闭连接
+        /// </summary>
+        public void Uninit()
+        {
+            if (m_socket != null)
+            {
+                if (m_socket.Connected)
+                {
+                    m_socket.Disconnect(false);
+                }
+                m_socket.Close();
+                m_socket = null;
+            }
+        }
+
+        public void SendData(string msg)
+        {
+            if (!isReady || msg == "")
+            {
+                return;
+            }
+            try
+            {
+                // \n\t\n是消息分隔符
+                byte[] data = Encoding.UTF8.GetBytes(msg + "\n\t\n");
+                m_socket.BeginSend(data, 0, data.Length, SocketFlags.None, result =>
+                {
+                    // 发送成功
+                    m_socket.EndSend(result);
+                }, null);
+            }
+            catch (Exception e)
+            {
+                // 发送失败
+                m_errorCallback?.Invoke($"消息发送失败: [{e.GetType()}]{e.Message}");
+            }
+        }
     }
 }
